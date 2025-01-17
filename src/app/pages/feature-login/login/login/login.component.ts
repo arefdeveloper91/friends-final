7cd737dd97d74bd0f7f34c2c779f4fb67c873167
Aref Chucri <arefchucri@MacBook-Air-de-Aref.local>
2025-01-17 17:04:29 -0300

    criado a pagina de login.ts

--- src/app/pages/feature-login/login/login/login.component.ts
+++ src/app/pages/feature-login/login/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+loginForm! : FormGroup;
+constructor(
 
+private router: Router,
+private LoginService:LoginService,
+private toastr: ToastrService
+
+
+){
+  this.loginForm = new FormGroup({
+    email: new FormControl('',[Validators.required,Validators.email]),
+    password: new FormControl('',[Validators.required,Validators.minLength(6)])
+
+  })
 }
+}
+
+Submit() {
+  this.LoginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe
+  (response => {
+    if(response.sucesso ==true){
+      this.toastr.success('login efetuado com sucesso')
+      this.router.navigate(["/home"])
+    }else{
+      this.toastr.error("falha no login, tentar novamente")
+    }
+  });
+navigate(){
+  this.router.navigate(["signup"])
+}
+navigateForgot(){
+  this.router.navigate(["forgot-password"])
+}
+}
+
