86e8624a3c642009a9eddf9f22f988d2d92bfa4c
Aref Chucri <arefchucri@MacBook-Air-de-Aref.local>
2025-01-17 16:06:39 -0300

    primeiros passos do projeto

---
+++ src/app/components/primary-input/primary-input.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PrimaryInputComponent } from './primary-input/primary-input.component';
+
+
+
+@NgModule({
+  declarations: [
+    PrimaryInputComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PrimaryInputModule { }
---
+++ src/app/components/primary-input/primary-input/primary-input.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primary-input',
+  templateUrl: './primary-input.component.html',
+  styleUrls: ['./primary-input.component.css']
+})
+export class PrimaryInputComponent {
+
+}
