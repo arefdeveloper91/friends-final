ce303e34da081015bf696491f67c3f9b6bd47371
Aref Chucri <arefchucri@MacBook-Air-de-Aref.local>
2025-01-17 16:42:46 -0300

    criando mais um modulo e um component

---
+++ src/app/components/default-login-layout/default-login-layout.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DefaultLoginLayoutComponent } from './default-login-layout/default-login-layout.component';
+
+
+
+@NgModule({
+  declarations: [
+    DefaultLoginLayoutComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DefaultLoginLayoutModule { }
